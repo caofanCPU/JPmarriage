dc6300f42418f91f7fd26a5ec56bbe19a0a6fa78
caofan <caofan@ovfintech.com>
2018-01-08 10:04:14 +0800

    the first successful version

--- src/main/java/Basic/JavaApplyPythonCodeTest.java
+++ src/main/java/Basic/JavaApplyPythonCodeTest.java
@@ -22,21 +22,20 @@ public class JavaApplyPythonCodeTest
         props.put("python.security.respectJavaAccessibility", "false");
         props.put("python.import.site", "false");
         Properties properties = System.getProperties();
-
         PythonInterpreter.initialize(properties, props, new String[0]);
         // 开启Python解释器
         PythonInterpreter pythonInterpreter = new PythonInterpreter();
         // Java直接执行Python语句
         pythonInterpreter.exec("print 'Hello World!'");
-//        // Java执行Python脚本
-//        String basePath = JavaApplyPythonCodeTest.class.getResource("").getPath();
-//        pythonInterpreter.execfile(basePath +"python_func.py");
-//        // Java执行Python函数
-//        PyFunction func = pythonInterpreter.get("add", PyFunction.class);
-//        int a = 1, b =2;
-//        PyObject pyobj = func.__call__(new PyInteger(a), new PyInteger(b));
-//        System.out.println(pyobj.toString());
-//        // 关闭Python解释器
+        // Java执行Python脚本
+        String basePath = JavaApplyPythonCodeTest.class.getResource("").getPath();
+        pythonInterpreter.execfile(basePath + "/" + "AddFunction.py");
+        // Java执行Python函数
+        PyFunction func = pythonInterpreter.get("add", PyFunction.class);
+        int a = 1, b =2;
+        PyObject pyobj = func.__call__(new PyInteger(a), new PyInteger(b));
+        System.out.println(pyobj.toString());
+        // 关闭Python解释器
         pythonInterpreter.close();
     }
 
